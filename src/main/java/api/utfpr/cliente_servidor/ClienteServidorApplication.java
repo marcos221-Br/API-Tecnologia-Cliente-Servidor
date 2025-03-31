5c85067f15d23a6addb1787af86aeb56162d29e3
Marcos Gomes <marcosartemio221@gmail.com>
2025-03-31 19:02:03 -0300

    Initial Commit

---
+++ src/main/java/api/utfpr/cliente_servidor/ClienteServidorApplication.java
@@ -0,0 +1,13 @@
+package api.utfpr.cliente_servidor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteServidorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteServidorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/utfpr/cliente_servidor/ClienteServidorApplicationTests.java
@@ -0,0 +1,13 @@
+package api.utfpr.cliente_servidor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteServidorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
